27c30821b1f5211c9e73ce4c9a484e424fedb62e
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-10-22 11:50:20 +0200

    Fichero JSON Added

--- testImdb.js
+++ testImdb.js
@@ -2,8 +2,12 @@
 exports.__esModule = true;
 var Imdb_1 = require("./Imdb");
 var movie_1 = require("./movie");
+var fs = require('fs');
 var movie1 = new movie_1.Movie("Intocable", 2014, "France", "Dranma");
 var movie2 = new movie_1.Movie("Intocable", 2014, "France", "Dranma");
 var movie3 = new movie_1.Movie("Intocable", 2014, "France", "Dranma");
 var movie4 = new movie_1.Movie("Intocable", 2014, "France", "Dranma");
-var Imdb1 = new Imdb_1.Imdb([movie1, movie2, movie3, movie3]);
+var imdb1 = new Imdb_1.Imdb([movie1, movie2, movie3, movie3]);
+console.log(JSON.stringify(imdb1));
+var objson = JSON.stringify(imdb1);
+fs.writeFileSync("imdbBBDD.json", objson);
--- testImdb.ts
+++ testImdb.ts
@@ -2,11 +2,19 @@ import { Imdb } from "./Imdb";
 import { Movie } from "./movie";
 import { Professional } from "./professional";
 
+const fs = require('fs');
+
 let movie1 = new Movie("Intocable", 2014, "France", "Dranma");
 let movie2 = new Movie("Intocable", 2014, "France", "Dranma");
 let movie3 = new Movie("Intocable", 2014, "France", "Dranma");
 let movie4 = new Movie("Intocable", 2014, "France", "Dranma");
 
-let Imdb1 = new Imdb([movie1, movie2, movie3, movie3])
+let imdb1 = new Imdb([movie1, movie2, movie3, movie3])
+
+console.log(JSON.stringify(imdb1));
+
+let objson = JSON.stringify(imdb1);
+
+fs.writeFileSync("imdbBBDD.json", objson);
 
   
